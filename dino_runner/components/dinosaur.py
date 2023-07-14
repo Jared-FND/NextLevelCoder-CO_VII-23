d0dac6173f210a1a52eda4c03f76828cdd40c5c9
juanjh1 <92651853+juanjh1@users.noreply.github.com>
2023-07-14 14:26:21 -0500

    class 1

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,10 @@
+#crear clase dynosaurio y sus metodos game lopp
+
+
+class Dinosaur():
+    def __init__(self) :
+        pass
+    def update(self):
+        pass
+    def draw(self):
+        pass
